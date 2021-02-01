57d1d925ffff3200c4a4152187ee4508ade5255e
Jen000 <60304399+Jen000@users.noreply.github.com>
2021-02-01 09:32:17 -0500

    Add files via upload

---
+++ help.py
@@ -0,0 +1,18 @@
+#import random module
+
+import random
+
+rand_num = (random.randint(1,10))
+#guess counter
+count = 1
+#ask user to guess, respond with directions
+#if guessed provide with the number and guesses.
+while True:
+	answer = int(input("Guess the random number\n"))
+	if answer == rand_num:
+		print ("You guessed it, the number was ", rand_num, "It only took", count, " guesses.")
+	elif answer < rand_num:
+		print ("Larger, guess again")
+	elif answer > rand_num:
+		print ("Samller, guess again")
+	count+=1
